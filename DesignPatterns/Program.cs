3dfba0d9cb097449d30924dc339e5396effa2953
Vigen Simonyan <vigen_simonyan@epam.com>
2023-06-09 15:50:32 +0400

    Decorator DP

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,1 +1,65 @@
-﻿//Navigate to other branches to see DPs impelementations
+﻿BaseAuto baseAuto = new BaseAuto();
+FerrariAutoDecorator ferrariAutoDecorator = new FerrariAutoDecorator();
+TeslaDecorator teslaDecorator = new TeslaDecorator();
+ferrariAutoDecorator.SetBaseAuto(baseAuto);
+teslaDecorator.SetBaseAuto(ferrariAutoDecorator);
+
+teslaDecorator.AddAutoParts();
+
+
+
+abstract class AutoPart
+{
+    public abstract void AddAutoParts();
+}
+
+
+class BaseAuto : AutoPart
+{
+    public override void AddAutoParts()
+    {
+        Console.WriteLine("Added Engine to a car");
+        Console.WriteLine("Added 4 tiers to a car");
+        Console.WriteLine("-------------------");
+    }
+}
+
+class BaseAutoDecorator : AutoPart
+{
+    protected AutoPart _wrapee;
+
+    public void SetBaseAuto(AutoPart wrapee)
+    {
+        _wrapee = wrapee;
+    }
+
+    public override void AddAutoParts()
+    {
+        if(_wrapee != null )
+        {
+            _wrapee.AddAutoParts(); 
+        }
+    }
+}
+
+class FerrariAutoDecorator : BaseAutoDecorator
+{
+    public override void AddAutoParts()
+    {
+        Console.WriteLine("Added very strong engine");
+        Console.WriteLine("Painted auto in red");
+        Console.WriteLine("-------------------");
+        base.AddAutoParts();
+    }
+}
+
+class TeslaDecorator : BaseAutoDecorator
+{
+    public override void AddAutoParts()
+    {
+        Console.WriteLine("Made engine electric");
+        Console.WriteLine("-------------------");
+        base.AddAutoParts();
+
+    }
+}
