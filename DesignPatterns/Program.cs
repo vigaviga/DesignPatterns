9296887491791c537327ba28dcc2c2479ff81200
Vigen Simonyan <vigen_simonyan@epam.com>
2023-06-12 10:09:49 +0400

    StrategyDP

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,1 +1,61 @@
-﻿//Navigate to other branches to see DPs impelementations
+﻿Context.SetBook("Novel");
+Context.Write();
+
+public interface IBookWriter
+{
+    void Write();
+}
+
+public class Fantasy : IBookWriter
+{
+    public void Write()
+    {
+        Console.WriteLine("I'm writting a fantasy book.");
+    }
+}
+
+
+public class Novel : IBookWriter
+{
+    public void Write()
+    {
+        Console.WriteLine("I'm writting a novel book");
+    }
+}
+
+public class Comedy : IBookWriter
+{
+    public void Write()
+    {
+        Console.WriteLine("I'm writting a comedy book.");
+    }
+}
+
+public class Context
+{
+    private static IBookWriter _book;
+
+    public static void SetBook(string type)
+    {
+        switch (type)
+        {
+            case "Novel":
+                _book = new Novel();
+                break;
+            case "Comedy":
+                _book = new Comedy();
+                break;
+            case "Fantasy":
+                _book = new Fantasy();
+                break;
+            default:
+                _book = new Novel();
+                break;
+        }
+    }
+
+    public static void Write()
+    {
+        _book.Write();
+    }
+}
