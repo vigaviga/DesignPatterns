05e7227e2aae31f766fe278c730e230a1d4702b5
Vigen Simonyan <vigen_simonyan@epam.com>
2023-06-12 11:22:08 +0400

    Template DP

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,1 +1,62 @@
-﻿//Navigate to other branches to see DPs impelementations
+﻿Food bbq = new BBQ();
+bbq.MakeFood();
+Console.WriteLine("------------------");
+Food salad = new Salad();
+salad.MakeFood();
+public abstract class Food
+{
+    public void MakeFood() 
+    {
+        BuyIngredients();
+        CleanIngredients();
+        CutIngredients();
+        MixIngredients();
+        CookIngredients();
+    }
+
+    protected virtual void BuyIngredients() 
+    {
+        Console.WriteLine("Bought ingredients.");
+    }
+
+    protected virtual void CleanIngredients()
+    {
+        Console.WriteLine("Cleaned ingredients.");
+    }
+
+    protected virtual void CutIngredients()
+    {
+        Console.WriteLine("Cut ingredients.");
+    }
+
+    protected virtual void MixIngredients()
+    {
+        Console.WriteLine("Mixed ingredients.");
+    }
+
+    protected virtual void CookIngredients()
+    {
+        Console.WriteLine("Cooked ingredients.");
+    }
+}
+
+public class BBQ : Food
+{
+    protected override void CleanIngredients()
+    {
+        Console.WriteLine("------------------");
+    }
+
+    protected override void MixIngredients()
+    {
+        Console.WriteLine("------------------");
+    }
+}
+
+public class Salad : Food
+{
+    protected override void CookIngredients()
+    {
+        Console.WriteLine("------------------");
+    }
+}
